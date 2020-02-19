bc3cfea38c97314ccc58c39a74c9cc359df64693
haolun <haolunhsu@gmail.com>
2020-02-19 10:45:33 -0600

    Remove duplicate lines

--- Projects/Proj01-ERModeling-SchemaDesign-BiddingDB/runParser.sh
+++ Projects/Proj01-ERModeling-SchemaDesign-BiddingDB/runParser.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 for i in {0..39}
 do
-    cat ebay_data/items-${i}.json_Bid.dat >> Bid.dat
-    cat ebay_data/items-${i}.json_Category.dat >> Category.dat
-    cat ebay_data/items-${i}.json_Items.dat >> Items.dat
-    cat ebay_data/items-${i}.json_Users.dat >> Users.dat
-done
-for i in {0..39}
-do
     python3 group8_parser.py ebay_data/items-${i}.json
 done
 touch Bid.dat
 touch Category.dat
 touch Items.dat
 touch Users.dat
+for i in {0..39}
+do
+    cat ebay_data/items-${i}.json_Bid.dat >> Bid.dat
+    cat ebay_data/items-${i}.json_Category.dat >> Category.dat
+    cat ebay_data/items-${i}.json_Items.dat >> Items.dat
+    cat ebay_data/items-${i}.json_Users.dat >> Users.dat
+done
+sort Bid.dat | uniq > Bid2.dat
+sort Category.dat | uniq > Category2.dat
+sort Items.dat | uniq > Items2.dat
+sort Users.dat | uniq > Users2.dat
+mv Bid2.dat Bid.dat
+mv Category2.dat Category.dat
+mv Items2.dat Items.dat
+mv Users2.dat Users.dat
 sqlite3 ebay.db < create.sql
 sqlite3 ebay.db < load.txt
