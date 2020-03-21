f398efef3e3e1c017ff07e09c918c6c4012bd51e
Chien-Ming Chen <chien-ming@rockhopper-01.cs.wisc.edu>
2020-03-21 12:18:17 -0500

    basic implementation of allocBuf, readPage and allocPage, Test 1 passed

--- Projects/Proj02-Badger-BufferManager/BufMgr/src/main.cpp
+++ Projects/Proj02-Badger-BufferManager/BufMgr/src/main.cpp
@@ -167,11 +167,11 @@ void testBufMgr()
 	//Commenting  a particular test requires commenting all tests that follow it else those tests would fail.
 	
 	fork_test(test1);
-	fork_test(test2);
-	fork_test(test3);
-	fork_test(test4);
-	fork_test(test5);
-	fork_test(test6);
+	//fork_test(test2);
+	//fork_test(test3);
+	//fork_test(test4);
+	//fork_test(test5);
+	//fork_test(test6);
 
 	//Close files before deleting them
 	file1.close();
@@ -229,7 +229,7 @@ void test2()
 		rid2 = page2->insertRecord(tmpbuf);
 
 		int index = random() % num;
-    pageno1 = pid[index];
+    	pageno1 = pid[index];
 		bufMgr->readPage(file1ptr, pageno1, page);
 		sprintf((char*)tmpbuf, "test.1 Page %d %7.1f", pageno1, (float)pageno1);
 		if(strncmp(page->getRecord(rid[index]).c_str(), tmpbuf, strlen(tmpbuf)) != 0)
